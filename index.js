bee5d9a822d322ee1d3ddf88317a35b2858f16a5
Alexandru Cazacu <alexandru.cazacu.9889@gmail.com>
2021-08-08 00:12:46 +0200

    Parse headers and add route handlers

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const StringDecoder = require("string_decoder").StringDecoder;
 
 const server = http.createServer((req, res) => {
   // Path
+  // TODO(Alex): Use non deprecated API
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
@@ -21,18 +22,64 @@ const server = http.createServer((req, res) => {
 
   // Payload
   const decoder = new StringDecoder("utf-8");
-  let buffer = '';
-  req.on('data', (data) => {
-      buffer += decoder.write(data);
-  })
+  let buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
 
-  // Send Response
-  res.end("Hello, World\n");
+  req.on("end", () => {
+    buffer += decoder.end();
 
-  // Log
-  console.log(`${method} ${trimmedPath}`, queryStringObject, headers);
+    // Chose handler
+    const chosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    const data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+
+    chosenHandler(data, (statusCode, paylaod) => {
+      statusCode = typeof statusCode === "number" ? statusCode : 200; // Default to 200
+      paylaod = typeof paylaod === "object" ? paylaod : {}; // Default to empy object
+
+      const payloadString = JSON.stringify(paylaod);
+
+      // Send Response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log
+      console.log(`Response: ${statusCode} '${payloadString}'`);
+      // console.log("BEGIN RESPONSE ------------------------------");
+      // console.log(`${method} '${trimmedPath}'`);
+      // console.log(queryStringObject);
+      // console.log(headers);
+      // console.log(buffer);
+      // console.log("END RESPONSE ------------------------------");
+    });
+  });
 });
 
 server.listen(3000, () => {
   console.log("The server is listening on port 3000");
 });
+
+const handlers = {};
+
+handlers.sample = (data, callback) => {
+  callback(406, { name: "sample handler" });
+};
+
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+const router = {
+  sample: handlers.sample,
+};
